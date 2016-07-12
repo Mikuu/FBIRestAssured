c983580eb3cc1d2e8e4583a44411c6a6c98f86ba
ariman <sherry_0678@163.com>
2016-07-12 18:01:26 +0800

    [Ariman]Initail

---
+++ src/test/java/com/restassured/demo/apitest/RequestData.java
@@ -0,0 +1,33 @@
+package com.restassured.demo.apitest;
+
+import java.util.List;
+
+public class RequestData {
+    private String range;
+    private String majorDimension;
+    private List values;
+
+    public String getRange() {
+        return this.range;
+    }
+
+    public String getMajorDimension() {
+        return this.majorDimension;
+    }
+
+    public List getValues() {
+        return this.values;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public void setMajorDimension(String majorDimension) {
+        this.majorDimension = majorDimension;
+    }
+
+    public void setValues(List values) {
+        this.values = values;
+    }
+}
